9bd4346f5d0178daa99f3ab00e57cf8f4a03285b
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-16 00:10:36 +0300

    Add form validation

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { User } from '../user';
 import { Repo } from '../repo';
+import { outputAst } from '@angular/compiler';
 
 @Component({
   selector: 'app-search-form',
@@ -9,10 +10,18 @@ import { Repo } from '../repo';
 })
 export class SearchFormComponent implements OnInit {
 
+  @Output() addUser = new EventEmitter<User>();
+  @Output() addRepo = new EventEmitter<Repo>();
+
+
   newUser = new User("","","",0,"",0,0,0,new Date());
   newRepo = new Repo("","","",0, new Date(), "");
 
-  
+  submitSearch(){
+    this.addUser.emit(this.newUser);
+    this.addRepo.emit(this.newRepo);
+
+  }
 
   constructor() { }
 
