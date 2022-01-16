0d44ee3a72608cfd0f8ced4bbcf1232ac0f3cf07
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-16 18:59:31 +0300

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component:UserComponent},
+  { path: 'repos', component:RepositoryComponent},
+  { path: 'form', component:SearchFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
