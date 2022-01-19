cdd887ed0eb68a1977db113e63132baadc262f5e
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-19 09:32:29 +0300

    Add screenshots from the app

--- src/app/duration.pipe.ts
+++ src/app/duration.pipe.ts
@@ -5,18 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DurationPipe implements PipeTransform {
 
-  transform(value:any): number {
-    let today:Date = new Date(); 
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(todayWithNoTime-value) 
-    const secondsInDay = 86400; 
-    var dateDifferenceSeconds = dateDifference*0.001; 
-    var daysAgo = dateDifferenceSeconds/secondsInDay;
-
-    if (daysAgo >= 1 && todayWithNoTime >value ){
-      return daysAgo;
-    }else{
-    return 0;
+  transform(value: any): any {
+    if (value) {
+      const seconds = Math.floor((+new Date() - +new Date(value)) *0.001);
+      if (seconds < 60)
+        return 'Just now';
+      const intervals: any = {
+        Years: 31536000,
+        Months: 2592000,
+        Weeks: 604800,
+        Days: 86400,
+        Hours: 3600,
+        Minutes: 60,
+        Seconds: 1,
+      };
+      let counter;
+      for (const i in intervals) {
+        counter = Math.floor(seconds / intervals[i]);
+        if (counter >=1)
+          return `${counter} ${i}`;
+      }
     }
+    return value;
   }
 }
+
